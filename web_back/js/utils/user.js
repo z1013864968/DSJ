268e28eea253a1a2469102ce6d6959ee581bdda1
zhou <1013864968@qq.com>
2019-09-21 19:49:25 +0800

    获取个人信息

--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -1,12 +1,29 @@
+
+
+
 var url='http://localhost:8000';
 var user={
+    /**
+     * 管理员登陆
+     * @param {*} userName 用户名
+     * @param {*} password 密码
+     * @param {*} callBack 回调函数
+     */
+    // 登陆
     login:function(userName,password,callBack){
         $.post(url+'/admin/login',{user_name:userName,password:password},function(res){
             callBack(res);
         })
     },
+    // 退出
     login_out:function(callBack){
         $.post(url+'/admin/logout',function(res){
+            callBack(res);
+        })
+    },
+    // 获取用户信息
+    login_users:function(callBack){
+        $.get(url+'/admin/getuser',function(res){
             callBack(res);
         })
     },
