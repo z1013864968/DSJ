3ea3801be9378694595fd2e70d5858bc66405b1c
zhou <1013864968@qq.com>
2019-09-23 21:47:11 +0800

    xialaliebiao

---
+++ web_back/js/utils/slug.js
@@ -0,0 +1,27 @@
+var url='http://localhost:8000';
+var slug={
+    search:function(callBack){
+        
+        $.get(url+'/admin/category_search',function(res){
+            callBack(res);
+        })
+       
+    },
+
+    add:function(name,slug,callBack){
+        $.post(url+'/admin/category_add',{name:name,slug:slug},function(res){
+            callBack(res);
+        })
+    },
+    del:function(id,callBack){
+        $.post(url+'/admin/category_delete',{id:id},function(res){
+            callBack(res);
+        })
+    },
+    edit:function(id,name,slug,callBack){
+        $.post(url+'/admin/category_edit',{id:id,name:name,slug:slug},function(res){
+            callBack(res);
+        })
+    },
+    
+}
